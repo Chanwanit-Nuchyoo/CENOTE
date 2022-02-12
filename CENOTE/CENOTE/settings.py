e7c63e08971f6a867c7f4c9eb0aa32c34369c638
Chanwanit-Nuchyoo <peaknuchyoo@gmail.com>
2022-02-12 14:31:03 +0700

    -add templates

--- CENOTE/CENOTE/settings.py
+++ CENOTE/CENOTE/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'CENOTE.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
