225142411de7d1d738b3ecff33c12d22149aaff5
Chanwanit-Nuchyoo <peaknuchyoo@gmail.com>
2022-02-17 13:41:08 +0700

    แก้อะไรนิดหน่อย

--- website/settings.py
+++ website/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
-    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,4 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('base.urls')),
-    path('',include('accounts.urls')),
 ]
