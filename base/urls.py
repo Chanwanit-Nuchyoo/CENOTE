75e67324e13f1824161c3af89efcdde831d16c7d
Captain <38762000+CaptainChuenthavorn@users.noreply.github.com>
2022-04-02 15:42:32 +0700

    add login&res path

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('opennote/',views.open, name='open'),
     path('book',views.book, name="book"),
+    path('register/', views.registerPage,name='register'),
+    path('login/', views.loginPage,name='login'),
+    
 ]
--- base/views.py
+++ base/views.py
@@ -5,11 +5,15 @@ from django.contrib.auth import authenticate, login
 def home(request):
     context = {}
     return render(request,'base/index.html',context)
-
+def registerPage(request):
+    context= {}
+    return render(request,'base/register.html',context)
+def loginPage(request):
+    context={}
+    return render(request,'base/login.html',context)
 def open(request):
     context = {}
     return render(request, 'base/open.html',context)
-
 def book(request):
     context = {}
     return render(request,'base/book.html',context)
