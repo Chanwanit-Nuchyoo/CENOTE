4a31f38291920cf7f08821b5ac11baa4cdafdcfd
Captain <38762000+CaptainChuenthavorn@users.noreply.github.com>
2022-04-24 23:42:21 +0700

    update gallery

--- base/static/base/gallery.js
+++ base/static/base/gallery.js
@@ -1,16 +1,24 @@
 let galleryImages=document.querySelectorAll(".gallery-img");
 let getLastestOpenedImg;
 let windowWidth = window.innerWidth;
-
+const fruits=[];
 if(galleryImages){
+
     galleryImages.forEach(function(image, index){
+        let getElementCssZ=window.getComputedStyle(image);
+        let getFullImgUrlZ=getElementCssZ.getPropertyValue("background-image");
+        let getImgUrlPosZ = getFullImgUrlZ.split("8000/");
+        
+        let SetNewImgUrlZ = getImgUrlPosZ[1].replace('")','');
+        fruits.push(SetNewImgUrlZ);
+        
         image.onclick=function() {
             let getElementCss=window.getComputedStyle(image);
             let getFullImgUrl=getElementCss.getPropertyValue("background-image");
             let getImgUrlPos = getFullImgUrl.split("8000/");
             
             let SetNewImgUrl = getImgUrlPos[1].replace('")','');
-            
+                
             getLastestOpenedImg = index+1;
 
             let container = document.body;
@@ -22,11 +30,71 @@ if(galleryImages){
             let newImg = document.createElement("img");
             newImgWindow.appendChild(newImg);
             newImg.setAttribute("src", "../"+SetNewImgUrl);
+            newImg.setAttribute("id", "current-img");
+
+            newImg.onload = function() {
+                let imgWidth = this.width;
+                let calcImgToEdge = ((windowWidth - imgWidth)/2)-90;
+
+                let newNextBtn = document.createElement("a");
+                let btnNextText = document.createTextNode("Next");
+                newNextBtn.appendChild(btnNextText);
+                container.appendChild(newNextBtn);
+                newNextBtn.setAttribute("class","img-btn-next");
+                newNextBtn.setAttribute("onclick","changeImg(1)");
+                newNextBtn.style.cssText="right: " + calcImgToEdge + "px;";
+                
+                let newPrevBtn = document.createElement("a");
+                let btnPrevText = document.createTextNode("Prev");
+                newPrevBtn.appendChild(btnPrevText);
+                container.appendChild(newPrevBtn);
+                newPrevBtn.setAttribute("class","img-btn-prev");
+                newPrevBtn.setAttribute("onclick","changeImg(0)");
+                newPrevBtn.style.cssText="left: " + calcImgToEdge + "px;";
+
+                // const img = document.createElement("img");
+                const img = new Image(100, 100); // width, heigh
+                img.src = "../static/base/right.png";
+                document.body.appendChild(img);
+            }
+            
+
         }
     });
 }
 
 function closeImg(){
     document.querySelector(".img-window").remove();
+    document.querySelector(".img-btn-next").remove();
+    document.querySelector(".img-btn-prev").remove();
 
+}
+
+function changeImg(changeDir){
+    // alert(fruits[0]);
+    document.querySelector("#current-img").remove();
+    let getImgWindow = document.querySelector(".img-window");
+    let newImg = document.createElement("img");
+    getImgWindow.appendChild(newImg);
+    
+    let calcNewImg;
+    if(changeDir === 1){
+        calcNewImg = getLastestOpenedImg +1;
+        if(calcNewImg > galleryImages.length) { 
+            calcNewImg = 1;
+        }
+    }
+    else if(changeDir ===0){
+        calcNewImg = getLastestOpenedImg - 1;
+        if(calcNewImg<1){
+            calcNewImg = galleryImages.length;
+        }
+    }
+    // alert(calcNewImg,fruits[calcNewImg]);  
+    // /media/images/captain/file_5lPcK6evlet.jpg
+    
+    newImg.setAttribute("src","../" + fruits[calcNewImg-1]);
+    newImg.setAttribute("id", "current-img");
+    getLastestOpenedImg = calcNewImg;
+    
 }
